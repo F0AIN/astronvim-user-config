61d96532f8c61f5e6b9b804f4d7d37b765ffd93d
frain <sin45kn704.coni@gmail.com>
2023-05-06 01:12:31 +0900

    Add: 補完機能に絵文字を追加

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,6 +2,28 @@
 return {
   { "dracula/vim" },
   {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji", -- add cmp source as dependency of cmp
+    },
+    opts = function(_, opts)
+      -- opts parameter is the default options table
+      -- the function is lazy loaded so cmp is able to be required
+      local cmp = require "cmp"
+      -- modify the sources part of the options table
+      opts.sources = cmp.config.sources {
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip",  priority = 750 },
+        { name = "buffer",   priority = 500 },
+        { name = "path",     priority = 250 },
+        { name = "emoji",    priority = 700 },   -- add new source
+      }
+
+      -- return the new table to be used
+      return opts
+    end,
+  },
+  {
     'renerocksai/telekasten.nvim',
     dependencies = {
       'nvim-telescope/telescope.nvim',
